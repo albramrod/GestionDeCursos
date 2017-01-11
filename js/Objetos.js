e1c1115f91d4ef1d7f37f10ba7bf0e50a295c777
Juan Jose <juanjose.fernandez.caro@gmail.com>
2017-01-11 01:01:13 +0100

    actualizacion de los objetos
    He añadido nuevos objetos

--- js/Objetos.js
+++ js/Objetos.js
@@ -1,7 +1,3 @@
-//---------- Objetos ----------
-
-
-
 //---------- Objeto Persona ----------
 function Persona(sDni,sNombre,sApellido,dFechaNacimiento, iTelefono,sDireccion){
 	
@@ -13,6 +9,7 @@ function Persona(sDni,sNombre,sApellido,dFechaNacimiento, iTelefono,sDireccion){
 	this.sDireccion=sDireccion;
 
 }
+
 //---------- Métodos Objeto Persona ----------
 Persona.prototype.toHTMLRow=function(){
 	
@@ -24,26 +21,90 @@ Persona.prototype.toHTMLRow=function(){
 	   		+ "<td>" + this.sDireccion + "</td>";
 
 }
+//********** Fin Objeto Persona **********
 
 //---------- Objeto Profesor ----------
-function Profesor (){
+function Profesor (sDni,sNombre,sApellido,dFechaNacimiento, iTelefono,sDireccion){
 	Persona.call(this,sDni,sNombre,sApellido,dFechaNacimiento, iTelefono,sDireccion);
 	this.aAsignatura = [ ];//array vacio que contine a objetos asignatura
 }
 
 Profesor.prototype = Object.create(Profesor.prototype);
 
-//---------- Objeto Profesor ----------
-function Alumno (){
+Profesor.prototype.toHTMLRow=function(){
+
+	var tdDatosProfesor=    "<td>" + this.sDni + "</td>"
+					 	  + "<td>" + this.sNombre + "</td>"
+	  					  + "<td>" + this.sApellido + "</td>"
+	   				 	  + "<td>" + this.dFechaNacimiento + "</td>"
+	   				 	  + "<td>" + this.iTelefono + "</td>"
+	   				 	  + "<td>" + this.sDireccion + "</td>";
+	
+	
+
+	//recorremos las asignaturas del profesor y almacenarlas en td
+	var tdDatosAsgnaturas = "<td>";
+	for (var i = 0; i < this.aAsignatura.length; i++) {
+	  		 	
+	    tdDatosAsgnaturas+= this.aAsignatura[i].sNombreAsignatura+"\n";
+	}  		
+	tdDatosAsgnaturas += "<td/>"; 
+
+	var tdProfesor= tdDatosProfesor+tdDatosAsgnaturas;
+
+
+	return tdProfesor;
+}
+//********** Fin Objeto Profesor **********
+
+//---------- Objeto Asignatura ----------
+function Asignatura (iIdAsignatura,sNombreAsignatura){
+	this.iIdAsignatura = iIdAsignatura;
+	this.sNombreAsignatura= sNombreAsignatura;
+}
+//---------- Métodos Objeto Asignatura ----------
+
+Asignatura.prototype.toHTMLRow=function(){
+
+	return    "<td>" + this.sNombreAsignatura + "</td>"; 
+
+}
+//********** Fin Objeto Asignatura **********
+
+
+//---------- Objeto Alumno ----------
+function Alumno (sDni,sNombre,sApellido,dFechaNacimiento, iTelefono,sDireccion){
 	Persona.call(this,sDni,sNombre,sApellido,dFechaNacimiento, iTelefono,sDireccion);
-	this.aNotas = [ ];//array vacio que contine a objetos asignatura
+	this.aNotas = [ ];//array vacio que contine a objetos Notas
 }
 
 //---------- Métodos Objeto ALumno ----------
 Alumno.prototype = Object.create(Alumno.prototype);
 
-Alumno.prototype.modificarNotaALumno = function(){
+Alumno.prototype.toHTMLRow = function(){
+
+	var tdDatosAlumno=    "<td>" + this.sDni + "</td>"
+					 	  + "<td>" + this.sNombre + "</td>"
+	  					  + "<td>" + this.sApellido + "</td>"
+	   				 	  + "<td>" + this.dFechaNacimiento + "</td>"
+	   				 	  + "<td>" + this.iTelefono + "</td>"
+	   				 	  + "<td>" + this.sDireccion + "</td>";
+	
+	
+
+	//recorremos las asignaturas del profesor y almacenarlas en td
+	var tdDatosNota = "<td>";
+	for (var i = 0; i < this.aNotas.length; i++) {
+	  		 	
+	    tdDatosNota+= this.aNotas[i].sNombreNota+"\n";
+	}  		
+	tdDatosNota += "<td/>"; 
+
+	var tdAlumno= tdDatosProfesor+tdDatosNota;
+
+
+	return tdProfesor;
 
 }
-
+//********** Fin Objeto Alumno **********
 
